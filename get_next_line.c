fa103998f6e32de98f4bef814ce46d58abda5881
jcherrerosl <jcherrerosl@gmail.com>
2024-09-26 18:22:07 +0200

    funciona

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: juanherr <juanherr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:04:35 by juanherr          #+#    #+#             */
-/*   Updated: 2024/09/25 16:56:05 by juanherr         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:16:52 by juanherr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,30 @@ char	*get_next_line(int fd)
 {
 	char		*buffer;
 	static char	*line;
-	size_t		readbytes;
+	ssize_t		readbytes;
 	size_t		i;
 	char		*aux;
 	char		*leftover;
 	char		*out;
 
-	if (fd < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+
 		return (NULL);
+	}
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buffer)
+	{
+//		printf("%ld\n", BUFFER_SIZE);
 		return (NULL);
-	line = NULL;
+	}
+//	line = NULL;
 	readbytes = read(fd, buffer, BUFFER_SIZE);
+	if (readbytes < 0)
+	{
+		free (buffer);
+		return (NULL);
+	}
 	while (readbytes > 0)
 	{
 		buffer[readbytes] = '\0';
@@ -37,13 +48,18 @@ char	*get_next_line(int fd)
 		aux = ft_strjoin(line, buffer);
 		free(line);
 		line = aux;
-		if (ft_strchr(buffer, '\n'))
+//		free(aux);
+		if (ft_strchr(line, '\n'))
 			break ;
 		readbytes = read(fd, buffer, BUFFER_SIZE);
 	}
 	free(buffer);
 	i = 0;
-	while (line[i] != '\n' && line[i])
+	if (!line)
+	{
+		line = ft_strdup("");
+	}
+	while (line[i] && line[i] != '\n')
 		i++;
 	if (line[i] == '\n')
 	{
@@ -51,6 +67,7 @@ char	*get_next_line(int fd)
 		leftover = ft_strdup(line + i + 1);
 		free (line);
 		line = leftover;
+		//free (leftover);
 		return (out);
 	}
 	out = ft_strdup(line);
@@ -59,20 +76,30 @@ char	*get_next_line(int fd)
 	if (ft_strlen(out) == 0)
 	{
 		free (out);
-		return (NULL);
+		out = NULL;
 	}
-	else
-		return (out);
+	return (out);
 }
 
-
+/*
 int	main(void)
 {
 	int	fd;
-	char	*line;
+	char *line;
+	size_t	i = 0;
 	
 	fd = open("texto.txt", O_RDONLY);
 	line = get_next_line(fd);
+	
+	//ft_putstr_fd(line, 1);
+	
+	//while (i < 3)
+	//{
+	//	printf("%s", line);
+	//	i++;
+	//	line = get_next_line(fd);
+	//}
+	
 	while (line)
 	{
 		printf("%s", line);
@@ -82,3 +109,4 @@ int	main(void)
 	close(fd);
 	return (0);
 }
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: juanherr <juanherr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:04:15 by juanherr          #+#    #+#             */
-/*   Updated: 2024/09/25 15:47:03 by juanherr         ###   ########.fr       */
+/*   Updated: 2024/09/26 15:20:20 by juanherr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stddef.h>
+# include <stdio.h>
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -33,5 +34,6 @@ void	ft_putendl_fd(char *s, int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strndup(const char *s, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
 
 #endif
