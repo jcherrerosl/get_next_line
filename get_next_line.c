ff5fa7385e2f25e5cb147396c0110b6970368384
Juan Carlos <121610967+jcherrerosl@users.noreply.github.com>
2024-09-26 02:20:17 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -19,42 +19,66 @@ char	*get_next_line(int fd)
 	size_t		readbytes;
 	size_t		i;
 	char		*aux;
+    char        *leftover;
+    char        *out;
 
 	if (fd < 0)
 		return (NULL);
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
+    line = NULL;
 	readbytes = read(fd, buffer, BUFFER_SIZE);
-	if (readbytes <= 0)
-	{
-		free (buffer);
-		return (line);
-	}
 	while (readbytes > 0)
 	{
 		buffer[readbytes] = '\0';
 		if (!line)
-			line = ft_strdup(buffer);
-		else
-		{
-			aux = ft_strjoin(line, buffer);
-			free (line);
-			line = aux;
-		}
-		if (ft_strchr(line, '\n'))
-			break ;
-	}
-	return (line);
+			line = ft_strdup("");
+		aux = ft_strjoin(line, buffer);
+        free(line);
+        line = aux;
+        if (ft_strchr(buffer, '\n'))
+            break ;
+        readbytes = read(fd, buffer, BUFFER_SIZE);
+    }
+    free(buffer);
+    i = 0;
+    while (line[i] != '\n' && line[i])
+        i++;
+    if (line[i] == '\n')
+    {
+        out = ft_substr(line, 0, i + 1);
+        leftover = ft_strdup(line + i + 1);
+        free (line);
+        line = leftover;
+        return (out);
+    }
+    out = ft_strdup(line);
+    free (line);
+    line = NULL;
+    if (ft_strlen(out) == 0)
+    {
+        free (out);
+        return (NULL);
+    }
+    else
+        return (out);
 }
 
 
 int	main(void)
 {
 	int			fd;
+    char        *line;
 	
 	fd = open("texto.txt", O_RDONLY);
-	ft_putstr_fd(get_next_line(fd), 1);
+    line = get_next_line(fd);
+    while (line)
+    {
+	    printf("%s", line);
+        free (line);
+        line = get_next_line(fd);
+    }
 	close(fd);
 	return (0);
 }
